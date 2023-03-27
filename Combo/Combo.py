a402aac8e1fbbdd313e5a4396396927a649a2c7a
Jish123K <128099655+Jish123K@users.noreply.github.com>
2023-03-27 16:46:37 +0530

    Create Combo.py

---
+++ Combo/Combo.py
@@ -0,0 +1,45 @@
+import os
+import cv2
+from moviepy.editor import VideoFileClip, concatenate_videoclips
+
+def createCombo(ComboWindow, inputFolder, outputFile):
+    """
+    find all video files and combine it into one file
+    params:
+      ComboWindow : UI for the application, object of QWindow class
+      inputFolder : input files folder path
+      outputFile : path to store the video file created
+
+    output : Outputs a single video file with file name provided at the location given
+    """
+    input_files = []  # array to store names of input files
+    clip = []  # to store the combination of above files
+
+    # reading the path of input folder and output file name
+    inputFolder = str(inputFolder)
+    outputFile = str(outputFile)
+
+    ComboWindow.setComboStatusTipText('Creating Video.......')  # setting status on the ui
+
+    # retrieving file names
+    for fileInput in os.listdir(inputFolder):
+        if fileInput.endswith(".mp4"):
+            input_files.append(os.path.join(inputFolder, fileInput))
+    input_files = sorted(input_files, key=str.lower)
+    lenInputFiles = len(input_files)
+
+    i = 0
+    # appending file names
+    for i in range(0, lenInputFiles):
+        per = float(i + 1) / float(lenInputFiles)
+        ComboWindow.setComboProgress(round(per * 60))
+        clip.append(VideoFileClip(input_files[i]))
+
+    # get default fps for output video
+    myClip = cv2.VideoCapture(input_files[i])
+    fps = myClip.get(cv2.CAP_PROP_FPS)
+
+    # creating a video and writing it to the directory
+    final_clip = concatenate_videoclips(clip)
+    final_clip.write_videofile(os.path.join(inputFolder, outputFile), fps=fps)
+    ComboWindow.setComboProgress(100)
